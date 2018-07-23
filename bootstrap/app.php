4c9bf40db12c17836acd796c368dc0e111cdfccb
Yongseon Choi <hcy3bkj@gmail.com>
2018-07-23 11:12:43 -0700

    backup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,6 @@
 <?php
 
 session_start();
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
 
 require __DIR__ . "/../vendor/autoload.php";
 
@@ -25,6 +23,10 @@ $container['view'] = function($container) {
 	));
 
 	return $view;
+};
+
+$container['HomeController'] = function($container) {
+	return new \App\Controllers\HomeController($container);
 };
 
 require __DIR__ . "/../app/routes.php";
